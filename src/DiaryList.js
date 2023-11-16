88bea82a245cc3b676b7073767b6637251faf41b
Jung ji hyung <gw03176370@gmail.com>
2023-11-16 09:54:56 +0900

    feat : [6-5] React에서 배열 사용하기 3 - 데이터 삭제하기

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,13 +1,14 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ diaryList }) => {
+const DiaryList = ({ onDelete, diaryList }) => {
   return (
     <div className='DiaryList'>
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
+        {/* props drilling: 사용하지 않는 props을 내려준다.(추후 다룰예정) */}
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} />
+          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
         ))}
       </div>
     </div>
